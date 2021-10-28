ec344fe2dd2c3835642c2c0e67f2632309232817
vrushalisarfare <87807056+vrushalisarfare@users.noreply.github.com>
2021-10-28 13:45:22 +0530

    Update New.java

--- New.java
+++ New.java
@@ -1,1 +1,7 @@
-https://github.com/vrushalisarfare/Demo.git
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
